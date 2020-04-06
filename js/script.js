9a5db922a5169b714fd14c471b3f7bd15b09ab8c
Darshan Patel <darshanpatel25894@gmail.com>
2020-04-05 23:34:51 -0400

    Completed navigation bar

--- js/script.js
+++ js/script.js
@@ -117,4 +117,31 @@ $(document).ready(function() {
 
     });
 
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        var body = $('body');
+
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px")
+            body.addClass("fixedNav");
+        } else {
+            body.css("padding-top", 0)
+            body.removeClass("fixedNav");
+        }
+    }
+
+    $("#navigation li a").click(function(e) {
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({ scrollTop: targetPosition - 50}, "slow");
+    });
+
+
 });
